5930f05f28ac5c015c151276a3300655afaacd33
JuanAzure <jgutierrez_pe@outlook.com>
2020-04-23 21:17:11 -0500

    Agregar archivos de proyecto.

---
+++ WepApi/Controllers/UserProfileController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WepApi.Models;
+
+namespace WepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : Controller
+    {
+        private UserManager<ApplicationUser> _userManager;
+
+        public UserProfileController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Authorize]
+        //GET: /api/userProfile
+        public async Task<Object> GetUserProfile()
+        {
+            string userid = User.Claims.First(c => c.Type == "UserID").Value;
+            var user = await _userManager.FindByIdAsync(userid);
+            return new
+            {
+                user.FullName,
+                user.Email,
+                user.UserName
+            };
+        }
+    }
+}
